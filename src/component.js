8c41e3805adb1cefcd5f297af1615be9bf8ff9f8
SM10 <22535138+SM10@users.noreply.github.com>
2023-04-25 17:07:22 -0400

    switch branch

--- src/component.js
+++ src/component.js
@@ -6,9 +6,10 @@ function Task(title) {
     this.notes= null
     this.project = null
     this.iscomplete = null
+    this.id = null
 }
 
-const TaskView = (task, id) => {
+const TaskView = (id) => {
     const innerviews = {
         title : null,
         description : null,
@@ -24,8 +25,10 @@ const TaskView = (task, id) => {
 
     let savefunction = null
     let deletefunction = null
+    let regularview = null
+    let sideview = null
 
-    const CreateNode = function(task, propertyname, inputtype){
+    const CreateNode = function(id, propertyname, inputtype){
         let ret = document.createElement('div')
         ret.className = 'inputholder'
 
@@ -34,7 +37,6 @@ const TaskView = (task, id) => {
 
         innerviews[propertyname] = document.createElement('input')
         innerviews[propertyname].type = inputtype
-        innerviews[propertyname].textContent = task[propertyname]
         innerviews[propertyname].name = propertyname
         innerviews[propertyname].className = 'task-' + propertyname
         innerviews[propertyname].id = 'task-' + propertyname + '-' + id
@@ -45,7 +47,7 @@ const TaskView = (task, id) => {
         return ret
     }
 
-    const CreateRadio = function(propertyname, value){
+    const CreateRadio = function(id, propertyname, value){
         let ret = document.createElement('div')
         ret.className = 'radioholder'
 
@@ -75,10 +77,8 @@ const TaskView = (task, id) => {
         return innerviews.savebutton
     }
 
-    const OnSaveButtonClickedListener = function(onSaveButtonClicked){
-        innerviews.savebutton.removeEventListener('click', savefunction, false)
-        savefunction = onSaveButtonClicked
-        innerviews.savebutton.addEventListener('click', onSaveButtonClicked, false)
+    const SetOnSaveButtonClickedListener = function(){
+        controller.OnSave()
     }
 
     const CreateDeleteButton = function(formid){
@@ -90,224 +90,52 @@ const TaskView = (task, id) => {
         return innerviews.deletebutton
     }
 
-    const OnDeleteButtonClickedListener = function(onDeleteButtonClicked){
+    const SetOnDeleteButtonClickedListener = function(onDeleteButtonClicked){
         innerviews.deletebutton.removeEventListener('click', deletefunction, false)
         deletefunction = onDeleteButtonClicked
         innerviews.deletebutton.addEventListener('click', deletefunction, false)
     }
 
-    const RegularView = () => {
+    const GetRegularView = () => {
+        if(regularview == null){
         let card = document.createElement('div')
         card.className = 'task-card task-regular-card'
         card.id = id + '-regular-card'
         
         let form = document.createElement('form')
         form.className = 'task-form'
-        form.id = id
+        form.id = id + '-form'
         card.appendChild(form)
 
-        form.appendChild(CreateNode(task, 'title', 'text'))
-        form.appendChild(CreateNode(task, 'description', 'text'))
-        form.appendChild(CreateNode(task, 'dueDate', 'date'))
-        form.appendChild(CreateRadio(task, 'priority', 'high'))
-        form.appendChild(CreateRadio(task, 'priority', 'normal'))
-        form.appendChild(CreateRadio(task, 'priority', 'low'))
-        form.appendChild(CreateNode(task, 'notes', 'text'))
-        form.appendChild(CreateNode(task, 'iscomplete', 'checkbox'))
+        form.appendChild(CreateNode(id + 'title', 'title', 'text'))
+        form.appendChild(CreateNode(id  + 'description', 'description', 'text'))
+        form.appendChild(CreateNode(id + 'dueDate', 'dueDate', 'date'))
+        form.appendChild(CreateRadio(id + 'highpriority', 'priority', 'high'))
+        form.appendChild(CreateRadio(id  + 'normalpriority', 'priority', 'normal'))
+        form.appendChild(CreateRadio(id  + 'lowpriority', 'priority', 'low'))
+        form.appendChild(CreateNode(id  + 'notes', 'notes', 'text'))
+        form.appendChild(CreateNode(id  + 'iscomplete', 'iscomplete', 'checkbox'))
         form.appendChild(CreateSaveButton(form.id))
         form.appendChild(CreateDeleteButton(form.id))
 
-        return card
-    }
-    const SidebarView = () => {
-        let card = document.createElement('div')
-        card.className = 'task-card task-side-card'
-        card.textContent = task.title
-        card.id = id + '-side-card'
+        regularview = card
 
         return card
+        }
+        
+        return regularview
+    }
+    const GetSidebarView = () => {
+        if(sideview == null){
+        let card = document.createElement('div')
+        card.className = 'task-card task-side-card'
+        card.id = id + '-side-card'
+
+        sideview = card
+        return card
+        }
+        return sideview
     }
     return {RegularView, SidebarView, OnSaveButtonClickedListener, OnDeleteButtonClickedListener}
 }
 
-function Project(name){
-    this.tasks = []
-    this.name = name
-
-    function GetTaskId(task){
-        for (let i = 0; i < this.tasks.length; i++){
-            if(this.tasks[i] == task) {return i}
-        }
-        return null
-    }
-
-    function AddTask(task){
-        this.tasks.push(task)
-    }
-
-    function RemoveTaskByObject(task){
-        for(let i = 0; i < this.tasks.length; i++){
-            if(this.tasks[i] === task) {this.tasks.splice(i, 0)}
-        }
-    }
-
-    function RemoveTaskByIndex(index){
-        this.tasks.splice(index,0)
-    }
-}
-
-const ProjectView = (project, id) => {
-
-    const RegularView = () => {
-        let projectpage = document.createElement('div')
-        projectpage.classname = 'project-regular-card'
-        projectpage.id = project + '-' + id + '-page'
-        
-        let projecttitle = document.createElement('div')
-        projecttitle.textContent = project.name
-        projectpage.appendChild(projecttitle)
-
-        let addtaskbutton = document.createElement('button')
-        addtaskbutton.textContent = 'Add Task'
-        addtaskbutton.className = 'add-task-button project-lv-button'
-        addtaskbutton.id = project + '-' + id + '-add-task'
-        projectpage.appendChild(addtaskbutton)
-
-        let deleteproject = document.createElement('button')
-        deleteproject.textContent = 'Delete Project'
-        deleteproject.className = 'add-task-button project-lv-button'
-        deleteproject.id = project + '-' + id + '-delete-project'
-        projectpage.appendChild(deleteproject)
-
-        let expandicon = document.createElement('img')
-        expandicon.className = 'project-lv-icon'
-        expandicon.id = project + '-' + id + '-expand-project'
-        projectpage.appendChild(expandicon)
-
-        project.tasks.forEach((task)=>{
-            projectpage.appendChild(TaskView(task).RegularView())
-        })
-
-        return projectpage
-    }
-
-    const SidebarView = () => {
-        let projectpage = document.createElement('div')
-        projectpage.classname = 'project-side-card'
-
-        let expandicon = document.createElement('img')
-        expandicon.className = 'project-lv-icon'
-        expandicon.id = project + '-' + id + '-expand-project-side'
-        projectpage.appendChild(expandicon)
-
-        let projecttitle = document.createElement('div')
-        projecttitle.textContent = project.name
-        project.tasks.forEach((task)=>{
-            projectpage.appendChild(TaskView(task).SidebarView())
-        })
-    }
-
-    const OnTaskAdded = (task) => {
-        let projectpage = document.querySelector('#' + project + '-' + id + '-page')
-        projectpage.appendChild(TaskView(task).RegularView())
-    }
-
-    const OnTaskRemoved = (id) => {
-        let projectpage = document.querySelector('#' + project + '-' + id + '-page')
-        projectpage.removeChild(document.querySelector('#' + id + '-regular-card'))
-    }
-
-    return {RegularView, SidebarView, OnTaskAdded, OnTaskRemoved}
-}
-
-const ProjectController = (project) => {
-    const AddTask = (task) => {
-        project.AddTask(task)
-    }
-
-    let OnTaskAdded = null;
-
-    const RemoveTaskByObject = (task) => {
-        project.RemoveTaskByObject(task)
-    }
-
-    let OnTaskRemoved = null;
-
-    const RemoveTaskByIndex = (index) =>{
-        project.RemoveTaskByIndex(index)
-    }
-
-    return {AddTask, RemoveTaskByObject, RemoveTaskByIndex, OnTaskAdded, OnTaskRemoved}
-}
-
-function Portfolio(name){
-    this.projects = []
-    this.name = name
-    function GetProjectId(project){
-        for (let i = 0; i < this.projects.length; i++){
-            if(this.projects[i] == project) {return i}
-        }
-        return null
-    }
-
-    function AddProject(project){
-        this.projects.push(project)
-    }
-
-    function RemoveProjectByObject(project){
-        for (let i = 0; i < this.projects.length; i++){
-            if(this.projects[i] == project) {this.projects.splice(1, 0)}
-        }
-    }
-
-    function RemoveProjectByIndex(index){
-        this.projects.splice(index, 0 )
-    }
-}
-
-const PortfolioController = (portfolio) => {
-    const AddProject = (project) => {
-        portfolio.AddProject(project)
-    }
-
-    let OnProjectAdded = null;
-
-    const RemoveProjectByObject = (project) => {
-        portfolio.RemoveProjectByObject(project)
-    }
-
-    const RemoveProjectByIndex = (index) => {
-        portfolio.projects.splice(index, 0 )
-    }
-
-    return {AddProject, RemoveProjectByObject, RemoveProjectByIndex, OnProjectAdded}
-}
-
-const PortfolioView = (portfolio) => {
-
-    const RegularView = () => {
-        let portfoliopage = document.createElement('div')
-        portfoliopage.id = portfolio.name + '-page'
-
-        portfolio.projects.forEach((project)=>{
-            portfoliopage.appendChild(ProjectView(project))
-        })
-
-        return portfoliopage
-    }
-
-    const OnProjectAdded = (project) => {
-        let portfoliopage = document.querySelector('#' + portfolio.name + '-page')
-        portfoliopage.appendChild(ProjectView(project))
-    }
-
-    const OnProjectRemoved = (id) => {
-        let projectpage = document.querySelector('#' + portfolio.name + '-page')
-        projectpage.removeChild(document.querySelector('#' + id + '-regular-card'))
-    }
-
-    return {RegularView, OnProjectAdded, OnProjectRemoved}
-}
-
-
-export {Task, TaskView, Project, ProjectView, ProjectController, Portfolio, PortfolioController, PortfolioView}
